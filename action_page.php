a020b21451c79957afd6061e96477bd4808561e7
jejakriot <khairilkamsaini196@gmail.com>
2022-05-17 14:47:06 +0800

    Added new files

---
+++ action_page.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+!hola¡ <?php echo $_POST["username"];?>, <br>
+ <?php echo $_POST["favskin"]; ?>. Good choice !
+<p>br0 its 2022 and u got rickrolled, you shoulda seen the look on yo face </p>
+
+<center>
+<video width="1500" height="auto" controls autoplay>
+  <source src="rickroll.mp4" type="video/mp4">
+  <source src="rickroll.ogg" type="video/ogg">
+  Your browser does not support the video tag.
+</video>
+<p>go<a href = "yourself.htm"> back </a>lol</p>
+
+
+<body style="background-color:#EBF0C2;">
+
+</body>
+</html>
